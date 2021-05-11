a345a414dfb872e137e13355bf9021fce2c557b0
AntonLazarev <bnnrymndy@gmail.com>
2021-05-11 15:33:10 +0300

    minor changes

--- AddLab.php
+++ AddLab.php
@@ -6,6 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/addLab.js"></script>
+
+    <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css"
+  rel="stylesheet"
+/>
+
     <title>Добавление лабораторной работы</title>
 </head>
 <body>
@@ -25,8 +42,8 @@
             <label for="rightAwnser">правильный ответ</label>
             <input name="rightAwnser" type="text">
         </fieldset>
-        <input id="button-0" class="addTask" onclick="addTask()" type="button" value="Добавить задание">
-
+        <input id="button-0" class="addTask" onclick="addTask()" type="button" value="Добавить задание"> 
+        <input disabled id="none-del"  onclick="delTask()" type="button" value="удалить задание">
         <input type="submit" value="Отправить">
     </form>
     </div>
--- js/AddLab.js
+++ js/AddLab.js
@@ -1,9 +1,23 @@
 var taskCounter = 0;
 
 function addTask() {
+    if (!taskCounter) {
+        $("#none-del").attr("disabled", false);
+    }
     taskCounter += 1;
-    alert("#" + this.id);
-    $("#" + this.id).after("<div id=" + taskCounter + "></div>");
+
+    $("#button-0").before("<div id=" + taskCounter + "></div>");
     $("#" + taskCounter).load("templates/taskForm.html");
-    $(".addTask").last().attr("id", "button-" + taskCounter);
+    console.log(taskCounter);
+    // $(".addTask").last().attr("id", "button-" + taskCounter);
+
+}
+
+function delTask() {
+    if (taskCounter > 1) {
+        $("#" + taskCounter--).remove();
+    } else {
+        $("#" + taskCounter--).remove();
+        $("#none-del").attr("disabled", true);
+    }
 }
