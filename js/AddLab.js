6b9fb33363c24b014ff2c4212e4e7c307aa4e4b6
AntonLazarev <bnnrymndy@gmail.com>
2021-05-24 12:40:16 +0300

    added functions for ajax send form

--- js/AddLab.js
+++ js/AddLab.js
@@ -7,11 +7,10 @@ function addTask() {
     }
     taskCounter += 1;
 
-    $(".btn-group").before("<div class='card  m-2 p-3' id=" + taskCounter + "></div>");
+    $(".btn-group").before("<div class='card taskCard m-2 p-3' id=" + taskCounter + "></div>");
     $("#" + taskCounter).load("templates/taskForm.html");
     console.log(taskCounter);
     // $(".addTask").last().attr("id", "button-" + taskCounter);
-
 }
 
 function delTask() {
@@ -25,14 +24,42 @@ function delTask() {
 }
 
 function sendAllTasks(labId) {
+    $('.taskCard').each({
+        function() {
 
+            var taskDescription = $(this).children();
+            var answerType = $('#labClass').val;
+            var rightAnswer = $('#releaseDate').val;
+
+
+            $.ajax({
+                type: "post",
+                url: "php/addTask.php",
+                data: {
+                    labId: labId,
+                    taskDescription: taskDescription,
+                    rightAnswer: rightAnswer
+                },
+                success: function(data) {
+                    // sendAllTasks(data);
+                }
+            });
+        }
+    });
 }
 
-function sendTask(labId) {
+function sendTask(labId, cardId) {
 
 }
 
 function sendLab() {
+    var labNumber = $('#labNumber').val();
+    var labName = $('#labName').val();
+    var labClass = $('#labClass').val();
+    var releaseDate = $('#releaseDate').val();
+    var deadline = $('#deadline').val();
+    var Login = $('#Login').val();
+
     $.ajax({
         type: "post",
         url: "php/addLab.php",
@@ -46,6 +73,14 @@ function sendLab() {
         },
         success: function(data) {
             sendAllTasks(data);
+        }
+    });
+}
+
+function testChild() {
+    $('.taskCard').each({
+        function() {
+            $(this).children('#floatingTextarea').val();
         }
     });
 }
